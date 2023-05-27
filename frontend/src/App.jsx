a1188a4aabad31e74c5e8ed2a44c1d46b837875b
TheParadox20 <onyambusamson@gmail.com>
2023-05-27 14:39:19 +0300

    test api on heroku

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import {Progress, Button, Spinner} from 'flowbite-react'
 
 export default function App() {
   const [count, setCount] = useState(0)
-  fetch('https://solana-iot.herokuapp.com//balance?address=G5ySUnvRthoersCZZqkZwWPWn9JvHSqFoocsJivgUzyT').then(res => res.json()).then(data => console.log(data))
+  fetch('https://solana-iot.herokuapp.com/balance?address=G5ySUnvRthoersCZZqkZwWPWn9JvHSqFoocsJivgUzyT').then(res => res.json()).then(data => console.log(data))
 
   return (
     <div className='container mx-auto text-center'>
